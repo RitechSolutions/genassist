2ed0767bf6600e6e8b593c14214584cfbfabafcb
Eledi Dyrkaj <edyrkaj@ritech.co>
2026-01-22 13:45:12 +0100

    feat: enhance file preview functionality by adding file type handling and icons

--- frontend/src/views/ActiveConversations/common/ConversationEntryWrapper.tsx
+++ frontend/src/views/ActiveConversations/common/ConversationEntryWrapper.tsx
@@ -1,4 +1,21 @@
+import React from 'react';
 import { TranscriptEntry } from "@/interfaces/transcript.interface";
+import { FileIcon } from "lucide-react";
+import { FileText, FileJson, FileImage } from 'lucide-react';
+
+interface FileData {
+  url: string;
+  name: string;
+  id: string;
+  type: string;
+}
+
+const getFileIcon = (fileType: string): React.ReactElement => {
+  if (fileType.startsWith('image/')) return <FileImage size={24} color="#6D28D9" />;
+  if (fileType === 'application/pdf') return <FileText size={24} color="#B91C1C" />;
+  if (fileType === 'application/json') return <FileJson size={24} color="#1D4ED8" />;
+  return <FileText size={24} color="#4B5563" />;
+};
 
 
 /**
@@ -6,18 +23,26 @@ import { TranscriptEntry } from "@/interfaces/transcript.interface";
  * @param fileData - The file data
  * @returns The file preview component
  */
-function FilePreview({ fileData }: { fileData: { url: string, name: string } }) {
-  return <div className="flex flex-col items-start gap-2 cursor-pointer" onClick={() => window.open(fileData.url, '_blank')}>
-    <img className="w-20 h-12 object-cover" src={fileData.url} alt="Image" loading="lazy" />
-    <span className="text-xs text-muted-foreground">{fileData.name}</span>
-  </div>;
+function FilePreview({ fileData }: { fileData: FileData }) {
+  if (fileData.type && fileData.type.startsWith('image')) {
+    return <div className="flex flex-col items-start gap-2 cursor-pointer" onClick={() => window.open(fileData.url, '_blank')}>
+      <img className="w-20 h-12 object-cover" src={fileData.url} alt="Image" loading="lazy" />
+      <span className="text-xs text-muted-foreground">{fileData.name}</span>
+    </div>;
+  } else {
+    return <div className="flex flex-col items-start gap-2 cursor-pointer" onClick={() => window.open(fileData.url, '_blank')}>
+      {getFileIcon(fileData.type)}
+      <span className="text-xs text-muted-foreground">{fileData.name}</span>
+    </div>;
+  }
 }
 
 export function ConversationEntryWrapper({ entry }: { entry: TranscriptEntry }) {
   try {
-    if (entry.type === "image_url") {
-      const fileData = JSON.parse(entry.text);
-      return <FilePreview fileData={fileData} />;
+    if (entry.type === "file") {
+      const cleanJson = entry.text && entry.text.replace(/\\/g, '');
+      const fileData = cleanJson ? JSON.parse(cleanJson) : null;
+      return <FilePreview fileData={fileData as FileData} />;
     } else {
       return <div>{entry.text}</div>;
     }
